6fce91d76d30e1a50333175e1cc6bdc82e2f8286
Miguel Ángel Sainz Ayuso <sanmiansaay7@gmail.com>
2015-01-28 18:03:46 +0100

    Ejercicio 5.5.5

--- calculator.js
+++ calculator.js
@@ -1,7 +1,16 @@
 
 
-var elemento = document.getElementById("op");
-console.log(elemento);
+function sumar(id, id2){
+	var elemento = document.getElementById(id);
+	var texto = elemento.innerHTML;
+	var elemento2 = document.getElementById(id2).innerHTML;
+	var numeros = texto.split("+");
+	var resultado = parseInt(numeros[0]) + parseInt(numeros[1]);
+	
+    document.getElementById(id2).innerHTML = " = " + resultado.toString();
+    document.getElementById("pepe").style.visibility="hidden"	
+}
+
 
 
 
