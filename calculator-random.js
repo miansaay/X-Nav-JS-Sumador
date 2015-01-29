b73cb64daef6d22809e356621773a2eb398d5388
Miguel Angel Sainz Ayuso <miansaay@lambda15.aulas.gsyc.es>
2015-01-29 14:39:11 +0100

    Ejercicio 5.5.6 final

--- calculator-random.js
+++ calculator-random.js
@@ -6,21 +6,12 @@ function sumar(id, id2){
 	var resultado = parseInt(numeros[0]) + parseInt(numeros[1]);
 	
     document.getElementById(id2).innerHTML = " = " + resultado.toString();
-    document.getElementById("pepe").style.visibility="hidden"	
 }
 
 function random(id1, id2){
 	var elemento = document.getElementById(id1).innerHTML;
-	var numero1 = Math.floor((Math.random() * 10000) + 10); 
-	var numero2 = Math.floor((Math.random() * 10000) + 10); 
+	var numero1 = Math.floor((Math.random() * 10000) + 1); 
+	var numero2 = Math.floor((Math.random() * 10000) + 1); 
 	document.getElementById(id1).innerHTML = numero1.toString() + "+" + numero2.toString();
-
-	//var numeros = elemento.split("+");
-
-	//var resultado = parseInt(numeros[0]) + parseInt(numeros[1]);
-	//console.log(resultado);
-	//document.getElementById(id2).innerHTML = " = " + resultado.toString();
-
-	
-
+        document.getElementById(id2).innerHTML = "";	
 }
